09a9661b0d47cfe8ba09af42878ff5cf1bf3e71e
gioby <dalloliogm@gmail.com>
2008-10-22 15:06:19 +0200

    wrote a skeleton for unittest/PedTest

--- test/code/unittests/PedTest.py
+++ test/code/unittests/PedTest.py
@@ -4,11 +4,33 @@
 import unittest
 from pprint import pprint
 import os
+import logging
 
-import Ped
+from PopGen.Gio import Ped
 
-class PedTest(unittest.TestCase):
+class PedParserTest(unittest.TestCase):
     """A test class for the Ped parser module"""
     
     def setUp(self):
-        self.testfiles = os.listdir('test/data/ped')
+        self.parser = Ped.RecordParser() 
+        self.testfilespath = '../../data/ped/'
+        self.testfiles = os.listdir(self.testfilespath)
+        
+    def testParser(self):
+        for filename in self.testfiles:
+            pop = self.parser.parse(file(self.testfilespath + filename, 'r'))
+            # how to test now?
+    
+    def tearDown(self):
+        """
+        tear down any data used in tests
+        tearDown is called after each test function execution.
+        """
+
+        pass
+        
+        
+if __name__ == '__main__':
+    logging.basicConfig(level = logging.DEBUG)
+    unittest.main()
+    
