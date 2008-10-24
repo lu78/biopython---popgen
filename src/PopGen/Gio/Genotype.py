4d52a63846e8abe8c9c67c5f53f9005d76c35345
gioby <dalloliogm@gmail.com>
2008-10-24 15:12:43 +0200

    created a 'Genotype' object

---
+++ src/PopGen/Gio/Genotype.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# Copyright 2008 by Giovanni Dall'Olio.  All rights reserved.
+# This code is part of the Biopython distribution and governed by its
+# license.  Please see the LICENSE file that should have been included
+# as part of this package.
+
+"""
+Genotype object
+
+>>> import Individual, Marker
+>>> g = Genotype()
+"""
+
+
+class Genotype(object):
+    pass
