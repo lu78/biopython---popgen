017d5c5577474560ed5abfda91e8e8b9d2faf16b
gioby <dalloliogm@gmail.com>
2008-10-24 16:25:43 +0200

    added an 'add_genotype' and '_recalculate_freqs' method to Marker

--- src/PopGen/Gio/Marker.py
+++ src/PopGen/Gio/Marker.py
@@ -11,9 +11,9 @@ class Marker(object):
     # Note: would it be better to use 'Marker' or 'Locus'?
     
     >>> C10G = Marker('A130G')
-    >>> C10G.genotypes = [('A', 'A'), ('G', 'A')])
+    >>> C10G.genotypes = (('A', 'A'), ('G', 'A'))    # should be done via add_genotype
     >>> C10G.genotypes.append(('A', 'A'))
-
+    AttributeError: 'tuple' object has no attribute 'append'
     '''      
           
     def __init__(self, name = None):
@@ -22,16 +22,25 @@ class Marker(object):
             name = 'Un-named Marker'
         self.name = name
         self.position = ""  # should be a 'position' object. For now, just a description (e.g. chromosome 11 pos 23131)
-        self.genotypes = [] # list of genotypes object (e.g.: [('A', 'A'), ('G', 'A')])
+        self.genotypes = () # list of genotypes object (e.g.: [('A', 'A'), ('G', 'A')]) Should be an object
         self.original_strand = ''    # should be '+' or '-'
         self.references = ''         # gene name, associated diseases, etc..
         
         
-    def add_individual(self, individual):
-        '''
-        '''
-        pass
+    def add_genotype(self, genotype):
+        """
+        use this method when adding a genotype.
+        """
+        # should check genotype format here
         
+        self.genotypes = (self.genotypes, genotype)
+        
+        self._recalculate_freqs() 
+        
+    def _recalculate_freqs(self):
+        """
+        recalculate allele frequency, total population, missing_data_count, etc...
+        """ 
         
 def _test():
     import doctest
