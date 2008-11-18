b7875526f0a1ad9d4927e439195a8b04d8c7e845
dalloliogm <dalloliogm@gmail.com>
2008-11-18 17:29:54 +0100

    simplified the sample hgdp file used in the doc of HGDPIO, hope I didn't introduce errors

--- src/PopGen/Gio/HgdpIO.py
+++ src/PopGen/Gio/HgdpIO.py
@@ -11,20 +11,20 @@ Example:
 ... BSGT Version    2.3.41.16318
 ... Processing Date    8/20/2006 1:40 PM
 ... Content        BDCHP-1X12-HUMANHAP650Y_11226216_A.csv
-... Num SNPs    655352
-... Total SNPs    655352
+... Num SNPs    19
+... Total SNPs    19
 ... Num Samples    30
 ... Total Samples    159
 ... [Data]
-...     WG0001019-DNAA03_NA18547    WG0001019-DNAD03_NA18593    WG0001019-DNAE03_NA18550    WG0001019-DNAF02_NA18577    WG0001019-DNAF03_NA18542    WG0001019-DNAG02_NA18537    WG0001019-DNAH02_NA18529    WG0001019-DNAH03_NA18576    WG0001019-DNAB03_NA18558    WG0001019-DNAC03_NA18612
+...     Ind1    Ind2    Ind3    Ind4    Ind5    Ind6    Ind7    Ind8    Ind9    Ind10
 ... MitoA10045G    AA    AA    AA    AA    AA    AA    AA    AA    AA    AA   
 ... MitoA10551G    AA    AA    AA    AA    AA    AA    AA    AA    AA    AA
 ... MitoA11252G    AA    AA    AA    AA    AA    AA    AA    AA    AA    AA
 ... MitoA11468G    AA    AA    AA    AA    AA    AA    AA    AA    AA    AA
-... MitoA11813G    AA    AA    AA    AA    AA    AA    AA    AA    AA    AA
+... MitoA11813G    AA    AA    AA    AA    AG    AA    AA    AA    AA    AA
 ... MitoA12309G    AA    AA    AA    AA    AA    AA    AA    AA    AA    AA
 ... MitoA13106G    AA    AA    AA    AA    AA    AA    AA    AA    AA    AA
-... MitoA13264G    GG    AA    AA    AA    GG    AA    AA    AA    AA    AA
+... MitoA13264G    GG    AA    AA    AA    GG    AG    AA    AA    AA    AA
 ... MitoA13781G    AA    AA    AA    AA    AA    AA    --    AA    AA    AA
 ... MitoA14234G    AA    AA    AA    AA    AA    AA    AA    AA    AA    AA
 ... MitoA14583G    AA    AA    AA    AA    AA    AA    AA    AA    AA    AA
@@ -34,7 +34,7 @@ Example:
 ... MitoA15302G    AA    AA    AA    AA    AA    GG    GG    AA    AA    GG
 ... MitoA15759G    AA    AA    AA    AA    AA    AA    AA    AA    AA    AA
 ... MitoA15908G    AA    AA    AA    AA    AA    AA    AA    AA    AA    AA
-... MitoA15925G    AA    AA    AA    AA    AA    AA    AA    AA    AA    AA
+... MitoA15925G    AA    AA    AA    AA    AA    AA    AA    AA    AG    AA
 ... MitoA16163G    AA    AA    AA    AA    AA    AA    AA    AA    AA    AA
 
 ... ''')
